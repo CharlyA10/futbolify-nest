7ceb95ef073ddb0239a8fd00f75e844dcf6aeb86
charly <alvaradohernandezc19@gmail.com>
2024-01-16 22:57:38 -0600

    eliminar

--- dist/projects/entities/projects.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { IProject } from '../../interfaces/project.interface';
-import { BaseEntity } from '../../config/base.entity';
-import { UsersProjectsEntity } from '../../users/entities/usersProjects.entity';
-export declare class ProjectsEntity extends BaseEntity implements IProject {
-    name: string;
-    description: string;
-    usersIncludes: UsersProjectsEntity[];
-}
